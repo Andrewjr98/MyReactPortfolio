981169cc9e32278c1f7909ff83e1f81a94214655
Andrew <andrewrandolph98@gmail.com>
2023-01-24 15:26:35 -0600

    First Commit

---
+++ src/componnents/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <ol>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+        </div>
+    )
+}
---
+++ src/componnents/pages/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <div>
+            <h1>Projects Page</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
